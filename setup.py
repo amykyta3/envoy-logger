2bd0145e8874fed5d5abb56056279f6450796e22
Alex Mykyta <amykyta3@users.noreply.github.com>
2022-11-28 21:40:48 -0800

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="enphase-power-logger",
-    version="1.0",
+    version="1.1",
     packages=setuptools.find_packages(exclude=["test"]),
     include_package_data=True,
     python_requires='>=3.6',
